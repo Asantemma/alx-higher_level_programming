d6897b4466adabcc57286c917a3016a25eba0661
ASANTE <asantemma7@gmail.com>
2023-06-11 23:20:30 +0000

    corrected whitespace error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
-    
+
     argc = len(argv)
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
