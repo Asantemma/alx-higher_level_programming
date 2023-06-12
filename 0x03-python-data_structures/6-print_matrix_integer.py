1c7d7cae289efd11221de4d1c48eab60aa45f7a1
ASANTE <asantemma7@gmail.com>
2023-06-12 23:23:41 +0000

    indentation error corrected

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     if matrix != [[]]:
+    if matrix != [[]]:
         for row in matrix:
             for column in row:
                 print("{:d}".format(column), end=" "
