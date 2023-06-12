8f124525a6c53080774179caec76400eeb6c1936
ASANTE <asantemma7@gmail.com>
2023-06-12 23:28:38 +0000

    spelling mistake corrected

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-     no_c_string = ""
+    no_c_string = ""
 
     for x, char in enumerate(my_string):
         if char != 'c' and char != 'C':
-            no_c_string = no_c_stribg + char
+            no_c_string = no_c_string + char
     return no_c_string
