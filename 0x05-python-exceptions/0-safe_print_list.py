f47322bd3891e0c0160bce1312e1a2ad7c4a2f0f
ASANTE <asantemma7@gmail.com>
2023-06-26 23:04:30 +0000

    Pushed safe_print_list file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    
+
     try:
         for a in range(x):
             print(my_list[a], end=' ')
             count += 1
     except IndexError:
         break
-    
-    print()  
-    
+    print()
+
     return count
