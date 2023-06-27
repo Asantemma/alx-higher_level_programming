2ddc7e3d18d2c4ff998e365205c0ff9f5d3bf56d
ASANTE <asantemma7@gmail.com>
2023-06-27 00:40:09 +0000

    Added task 8

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        print()  # Print a new line
+        return True
+    except (TypeError, ValueError) as er:
+        print("Exception: {}".format(er), file=sys.stderr)
+        return False
