ae4bdb86fb36733ba36d97076d72a4b7b51b336b
ASANTE <asantemma7@gmail.com>
2023-07-05 19:04:07 +0000

    added last file

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,75 +1,64 @@
 #!/usr/bin/python3
-""" Solves a Chess puzzle of proper positioning and backtracking"""
+"""
+N queens backtracking program to find solutions for placing N queens
+on an NxN grid such that they are all in non-attacking positions
+"""
 
 
 import sys
 
 
-def is_safe(board, row, col, N):
-    """Check if it's safe to place a queen at a given position."""
-    for i in range(row):
-        if board[i][col] == 'Q':
-            return False
+if __name__ == "__main__":
+    board = []
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    size = int(argv[1])
+    if size < 4:
+        print("N must be at least 4")
+        exit(1)
 
-    i = row - 1
-    j = col - 1
-    while i >= 0 and j >= 0:
-        if board[i][j] == 'Q':
-            return False
-        i -= 1
-        j -= 1
+#initializing the chessboard
+for i in range(size):
+    board.append([i, None])
 
-    i = row - 1
-    j = col + 1
-    while i >= 0 and j < N:
-        if board[i][j] == 'Q':
-            return False
-        i -= 1
-        j += 1
+def queen_exists_in_same_column(column):
+    """Checks if a queen already exists in the given column"""
+    for row in range(size):
+        if column == board[row][1]:
+            return True
+    return False
 
+def reject_placement(row, column):
+    """Determine whether or not to reject the queen placement"""
+    if queen_exists_in_same_column(column):
+        return False
+    i = 0
+    while i < row:
+        if abs(board[i][1] - column) == abs(i - row):
+            return False
+        i += 1
     return True
 
+def clear_board(row):
+    """Clear the board from the point of failure onwards"""
+    for i in range(row, size):
+        board[i][1] = None
 
-def solve_nqueens(N):
-    """Solve the N-Queens problem and return all possible answers."""
-    board = [['.' for r in range(N)] for r in range(N)]
-    answers = []
+def find_solutions(row):
+    """Recursive backtracking function to find the solutions"""
+    for column in range(size):
+        clear_board(row)
+        if reject_placement(row, column):
+            board[row][1] = column
+            if row == size - 1:
+                print(board)
+            else:
+                find_solutions(row + 1)
 
-    def backtrack(row):
-        """Backtracking function to place queens recursively."""
-        if row == N:
-            answers.append([''.join(row) for row in board])
-            return
+# Start the recursive process at the first row (row 0)
+find_solutions(0)
 
-        for col in range(N):
-            if is_safe(board, row, col, N):
-                board[row][col] = 'Q'
-                backtrack(row + 1)
-                board[row][col] = '.'
-
-    backtrack(0)
-
-    return answers
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    answers = solve_nqueens(N)
-
-    for answer in answers:
-        for row in answer:
-            print(row)
-        print()
