766111c7ff75db833c04c0beb3f0c7f177b8899d
ASANTE <asantemma7@gmail.com>
2023-07-09 15:03:40 +0000

    added another file

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -68,8 +68,8 @@ class Square:
         Raises:
             TypeError: If value is not a tuple of 2 positive integers.
         """
-        if not isinstance(value, tuple) or len(value) != 2 or \
-                not all(isinstance(num, int) and num >= 0 for num in value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) and num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -89,8 +89,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for x in range(self.__position[1]):
                 print()
 
-            for _ in range(self.__size):
+            for x in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
