97cf69b2bcd0bbbbed59abf6bed20eea76d0e52f
ASANTE <asantemma7@gmail.com>
2023-07-09 15:06:51 +0000

    added another file

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -87,10 +87,22 @@ class Square:
         Prints the square with the character # in stdout.
         """
         if self.__size == 0:
-            print()
-        else:
-            for x in range(self.__position[1]):
-                print()
+            print("")
+            return
 
-            for x in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
