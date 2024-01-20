f5d7fd20864fde18179cce882d401ba4539be27e
Asantemma <asantemma7@gmail.com>
2024-01-20 23:20:46 +0000

    Added SQL files

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script deletes the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 does not exists, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- This script lists all the tables of a database in your MySQL server.
+
+SHOW TABLES;
