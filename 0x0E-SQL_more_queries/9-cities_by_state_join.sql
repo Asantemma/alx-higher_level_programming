d1e8391602837977080b4c19be742ee3e425cae1
Asantemma <asantemma7@gmail.com>
2024-01-21 21:16:01 +0000

    Added reame.md file and mysql files

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- A script lists all cities contained in the database hbtn_0d_usa.
 
 SELECT cities.id, cities.name, states.name
-FROM cities c
+FROM cities
 JOIN states
-ON c.state_id = states.id;
+ON cities.state_id = states.id;
