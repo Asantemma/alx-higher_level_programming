45313672ab826cdcd6f4397d1caba7ce2d4a99ab
Emmanuel Asante <asantemma7@gmail.com>
2024-03-03 21:45:46 +0000

    Made shellcheck corrections

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, and display only body of a 200 status code response
+# Takes in a URL, sends a GET request to the URL, and display only body of 
+# a 200 status code response
 curl -s "$1" -X GET -L
